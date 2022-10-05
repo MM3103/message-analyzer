8a801dfcf3d223f20dc752799f9663c587c31610
MM3103 <misha.makhalin@yandex.ru>
2022-10-05 23:52:34 +0300

    Create message-analyzer

---
+++ core/src/main/java/liga/medical/medeicalmessageanalyzer/core/MedicalMessageAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package liga.medical.medeicalmessageanalyzer.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MedicalMessageAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalMessageAnalyzerApplication.class, args);
+    }
+}
---
+++ core/src/test/java/liga/medical/medeicalmessageanalyzer/core/MedicalMessageAnalyzerApplicationTest.java
@@ -0,0 +1,12 @@
+package liga.medical.medeicalmessageanalyzer.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MedicalMessageAnalyzerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
