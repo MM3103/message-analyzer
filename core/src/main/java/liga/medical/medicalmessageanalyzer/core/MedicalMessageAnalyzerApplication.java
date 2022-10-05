48cdf3b5614c45c9fb592b677df9d3bfaec2a8f1
MM3103 <misha.makhalin@yandex.ru>
2022-10-06 00:13:24 +0300

    hometask-2

--- core/src/main/java/liga/medical/medicalmessageanalyzer/core/MedicalMessageAnalyzerApplication.java
+++ core/src/main/java/liga/medical/medicalmessageanalyzer/core/MedicalMessageAnalyzerApplication.java
@@ -1,8 +1,7 @@
-package liga.medical.medeicalmessageanalyzer.core;
+package liga.medical.medicalmessageanalyzer.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class MedicalMessageAnalyzerApplication {
